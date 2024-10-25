634cc8098b4bcabc214896a854a426b16a4032b0
ĐỖ Ngọc Lộc <51218059+loc01227175513@users.noreply.github.com>
2024-10-25 09:26:48 +0700

    Delete duaninertia-themdb-factory-seeder/duanmoi directory

--- duaninertia-themdb-factory-seeder/duanmoi/routes/TestApi/TestApi.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TestApi\TestApi;
-
-Route::prefix('TestApi')->group(function () {
-    Route::get('TestApi', [TestApi::class, 'index'])->name('testapi');
-});
